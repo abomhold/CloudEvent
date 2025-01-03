d71ae9c3bd4ed1d17051f9d4309da8ab3c2b0755
austin <abomhold@uw.edu>
2025-01-02 17:29:11 -0800

    refactored lambda builder

--- CloudEventDeploy/src/main/java/Deploy/Resources/CloudConfigs.java
+++
@@ -1,9 +0,0 @@
-package Deploy.Resources;
-
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-public interface CloudConfigs {
-
-}
-
--- CloudEventDeploy/src/main/java/Deploy/ResourcesStack.java
+++
@@ -1,8 +0,0 @@
-package Deploy;
-
-import software.amazon.awssdk.services.lambda.model.Architecture;
-import software.amazon.awssdk.services.lambda.model.Runtime;
-
-interface ResourcesStack {
-
-}
